debdeaabcaed6fd1c70a4288cd8bad61e5260d8a
Animesh <241551094@kiit.ac.in>
2025-12-12 13:26:28 +0530

    1st

--- src/NAME.java
+++ src/NAME.java
@@ -1,9 +1,30 @@
-public class NAME {
+import java.util.Scanner;
+
+public class NAME{
     public static void main(String[] args) {
-        System.out.println("NAME-ANIMESH PAL");
-        System.out.println("ROLL-241551094");
-        System.out.println("SECTION-CSE 33");
-        System.out.println("BRANCH-CSE AI&ML");
+        Scanner sc = new Scanner(System.in);
+
+        // Input from user
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your roll number: ");
+        int roll = sc.nextInt();
+        sc.nextLine(); // clear buffer
+
+        System.out.print("Enter your branch: ");
+        String branch = sc.nextLine();
+
+        System.out.println("enter your section");
+        String section= sc.nextLine();
+
+        // Output
+        System.out.println("\n--- Student Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + roll);
+        System.out.println("Branch: " + branch);
+        System.out.println("section:" +section);
+
+        sc.close();
     }
 }
-
