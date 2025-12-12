28eb726782464bc5865258d34b8601202da8f0e1
Animesh <241551094@kiit.ac.in>
2025-12-12 12:51:54 +0530

    kiit_java_lab

---
+++ src/NAME.java
@@ -0,0 +1,8 @@
+public class NAME {
+    public static void main(String[] args) {
+        System.out.println("NAME-ANIMESH PAL");
+        System.out.println("ROLL-241551094");
+        System.out.println("SECTION-CSE 33");
+        System.out.println("BRANCH-CSE AI&ML");
+    }
+}
